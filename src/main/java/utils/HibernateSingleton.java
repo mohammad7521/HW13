90f565070b5f2f618d578c7398dc9c6416d454fb
MMD <m.mohammad.7521@gmail.com>
2022-03-06 05:51:20 +0330

    adding singleton session factory

---
+++ src/main/java/utils/HibernateSingleton.java
@@ -0,0 +1,34 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateSingleton {
+
+    private HibernateSingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure() // goes and fetches configuration from hibernate.cfg.xml
+                    .build();
+
+            // registry is useful for creating SessionFactory
+            // SessionFactory is a heavyweight object.
+            // SessionFactory is thread safe.
+            // SessionFactory is immutable.
+
+            INSTANCE = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
